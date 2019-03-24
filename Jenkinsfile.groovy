8988e4dd88ab756d78c4d7c5755506b704578659
azaa1 <azaa.chi11@gmail.com>
2019-03-24 21:15:30 +0000

    Jenkinsfile edited

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,4 +1,5 @@
 node{
+    properties([parameters([string(defaultValue: '104.248.7.62', description: 'Enter the server IP', name: 'IP', trim: false), string(defaultValue: 'latest', description: 'Enter the app version', name: 'VER', trim: false), string(defaultValue: '4000', description: 'Enter the port to run the app on ', name: 'PORT', trim: false)])])
     stage("Remove Container"){
         try{
             sh "ssh root@${IP} docker rm -f myapp"
@@ -9,7 +10,7 @@ node{
     }
 
     stage("Run Container"){
-        sh "ssh root@${IP}  docker run -d --name myapp -p 6000:4000 azaa1/python_app"
+        sh "ssh root@${IP}  docker run -d --name myapp -p ${PORT}:4000 azaa1/python_app:${VER}"
     }
 
 }
